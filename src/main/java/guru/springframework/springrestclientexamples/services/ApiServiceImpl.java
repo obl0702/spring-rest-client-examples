793fba341cd590940a9267e15a16c8475c0c337c
boonloong <obl0702@gmail.com>
2022-11-26 23:21:45 +0800

    Added Rest API Service. Closed #2

---
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -0,0 +1,24 @@
+package guru.springframework.springrestclientexamples.services;
+
+import guru.springframework.api.domain.User;
+import guru.springframework.api.domain.UserData;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class ApiServiceImpl implements ApiService{
+
+    private RestTemplate restTemplate;
+
+    public ApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<User> getUsers(Integer limit){
+        List<User> userData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+        return userData;
+    }
+
+}
