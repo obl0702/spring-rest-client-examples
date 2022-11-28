28d514f2411cf3b0cdf9047b01ed417b6c8cb6be
boonloong <obl0702@gmail.com>
2022-11-28 12:18:50 +0800

    Refactored to Reactive with WebClient. Closed #6

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -3,9 +3,13 @@ package guru.springframework.springrestclientexamples.services;
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -31,6 +35,17 @@ public class ApiServiceImpl implements ApiService{
         List<User> userData = restTemplate.getForObject(uriBuilder.toUriString(), List.class);
         return userData;
 
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient.create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("_limit", limit.toProcessor().block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(resp -> resp.bodyToMono(List.class))
+                .flatMapIterable(list -> list);
     }
 
 }
